3043c957879a159a2f9c9880a5f61c5504ed5dba
AadilParack <92970419+AadilParack@users.noreply.github.com>
2023-04-09 13:16:30 +0530

    Update game.py

--- game.py
+++ game.py
@@ -18,9 +18,6 @@ font = pygame.font.Font(None, 36)
 button_text = font.render("Play", True, (255, 255, 255))
 button_rect = button_text.get_rect(center=screen.get_rect().center)
 
-button_text2 = font.render("Play", True, (255, 255, 255))
-button_rect2 = button_text.get_rect(center=screen.get_rect().center)
-
 # Set up the game screen
 game_text = font.render("Welcome to the game!", True, (255, 255, 255))
 game_rect = game_text.get_rect(center=screen.get_rect().center)
@@ -36,11 +33,36 @@ scaled_image = pygame.transform.scale(image, scaled_size)
 # Get the rectangle for the scaled image
 scaled_rect = scaled_image.get_rect(center=screen.get_rect().center)
 
-print(image_rect)
+
+buttons = []
+buttons.append({"rect": pygame.Rect(400, 400, 100, 100), "text": (1,2)})
+buttons.append({"rect": pygame.Rect(515, 400, 100, 100), "text": (2,2)})
+buttons.append({"rect": pygame.Rect(285, 400, 50, 100), "text": (3,2)})
+buttons.append({"rect": pygame.Rect(400, 465, 100, 100), "text": (4,6)})
+buttons.append({"rect": pygame.Rect(400, 335, 100, 100), "text": (5,6)})
+
+board = [[0 for j in range(11)] for i in range(19)]
+
+def makeboard():
+    i=0
+    j=0
+    for i in range(19):
+        for j in range(11):
+            break
+    
+
+makeboard()
+
+def gamefunction(arg1):
+    print(arg1)
+
+def computerturn():
+    print("")
 
 # Main game loop
 running = True
 play_pressed = False
+playerturn = True
 while running:
     # Handle events
     for event in pygame.event.get():
@@ -54,15 +76,20 @@ while running:
     # If play has been pressed, show the game screen
     if play_pressed:
         # Clear the screen
-        #screen.fill(background_color)
-
-        # Draw the game screen
-        #screen.blit(game_text, game_rect)
-        #screen.blit(image, image_rect)
+        screen.fill(background_color)
         screen.blit(scaled_image, scaled_rect)
-        #gameloop
-        screen.blit(button_text2, button_rect2)
-
+        if playerturn:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    for button in buttons:
+                        if button["rect"].collidepoint(mouse_pos):
+                            gamefunction(button["text"])
+        else:
+            computerturn()
+            playerturn=True
+        
+        
     # If play hasn't been pressed, show the button
     else:
         # Draw the button
@@ -74,4 +101,6 @@ while running:
 
 # Quit Pygame
 pygame.quit()
+
+
 
